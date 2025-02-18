5272dfb553a6ce551601814835236c7bdfa2c960
Claire Sbaffe <claire.sbaffe@efrei.net>
2025-02-18 09:42:49 +0100

    Initial commit

---
+++ src/main/java/com/claire/NeoWs/Nasa/API/NeoWsNasaApiApplication.java
@@ -0,0 +1,13 @@
+package com.claire.NeoWs.Nasa.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoWsNasaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoWsNasaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claire/NeoWs/Nasa/API/NeoWsNasaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claire.NeoWs.Nasa.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoWsNasaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
